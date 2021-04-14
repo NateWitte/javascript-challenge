b494721d82cec22b9eb01101f48cd43481d9f9dc
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-04-13 22:53:25 -0500

    Correctly Reading Submitted Date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var text = d3.select("#datetime");
+function handleChange(event) {
+    // grab the value of the input field
+    var inputText = d3.event.target.value;
+  
+  
+    // Set the output text to the reversed input string
+    console.log(inputText);
+  }
+  
+  text.on("change", handleChange);
