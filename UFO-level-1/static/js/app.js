dc3d7c765efcda9946c77503ff898b19dd68a853
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-04-14 22:20:56 -0500

    Finished first part

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,14 +2,29 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var text = d3.select("#datetime");
 function handleChange(event) {
+    d3.event.preventDefault();
     // grab the value of the input field
     var inputText = d3.event.target.value;
-  
-  
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    data.forEach(function(weatherReport) {
+        console.log(weatherReport.datetime);
+        console.log(text);
+      if (weatherReport.datetime === inputText){
+        var row = tbody.append("tr");
+        Object.entries(weatherReport).forEach(function([key, value]) {
+        // Append a cell to the row for each value
+        // in the weather report object
+            var cell = row.append("td");
+            cell.text(value);
+        });
+        };
+    });
     // Set the output text to the reversed input string
     console.log(inputText);
   }
-  
+  var text = d3.select("#datetime");
   text.on("change", handleChange);
+// Step 5: Use d3 to update each cell's text with
+// weather report values (weekday, date, high, low)
