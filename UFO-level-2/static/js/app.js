68d5d15e48325919a38e004f63edc8075f53b1e3
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-04-14 23:35:46 -0500

    Did filter for other 4 cases

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -4,18 +4,32 @@ var tableData = data;
 // YOUR CODE HERE!
 function handleChange(event) {
     d3.event.preventDefault();
-    var date = d3.select("#datetime");
     // grab the value of the input field
-    var inputDate = date.property("value");
+    var inputDate = d3.select("#datetime").property("value");
+    var inputCity = d3.select("#city").property("value");
+    var inputState = d3.select("#state").property("value");
+    var inputCountry = d3.select("#country").property("value");
+    var inputShape = d3.select("#shape").property("value");
     filtereddata=data;
     if (inputDate){
       filtereddata=filtereddata.filter(sighting => sighting.datetime === inputDate);
     };
+    if (inputCity){
+      filtereddata=filtereddata.filter(sighting => sighting.city === inputCity);
+    };
+    if (inputState){
+      filtereddata=filtereddata.filter(sighting => sighting.state === inputState);
+    };
+    if (inputState){
+      filtereddata=filtereddata.filter(sighting => sighting.country === inputCountry);
+    };
+    if (inputShape){
+      filtereddata=filtereddata.filter(sighting => sighting.shape === inputShape);
+    };
     var tbody = d3.select("tbody");
     //clear table
     tbody.html("");
-    data.forEach(function(weatherReport) {
-      if (weatherReport.datetime === inputDate){
+    filtereddata.forEach(function(weatherReport) {
         var row = tbody.append("tr");
         Object.entries(weatherReport).forEach(function([key, value]) {
         // Append a cell to the row for each value
@@ -23,7 +37,6 @@ function handleChange(event) {
             var cell = row.append("td");
             cell.text(value);
         });
-        };
     });
   }
   var button = d3.select(".btn");
